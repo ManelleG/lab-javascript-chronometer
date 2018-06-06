9afa01cab7496be892fd189b80f19efb946ce3da
Manelle_G;` <manelle.gueriouz@gmail.com>
2018-06-06 18:26:24 +0200

    done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,39 +1,61 @@
 // Constructor
-// function Chronometer() {
+function Chronometer () {
+    this.currentTime = 0;
+    this.intervalId = 0;
+    this.minutes = "00";
+    this.seconds = "00";
+};
 
-// }
+Chronometer.prototype.startClick = function () {
+    // var that = this;
+    // this.intervalId = setInterval(function () {
+    //     that.currentTime += 1;
+    // }, 1000);
+    this.intervalId = setInterval (() => {
 
-// Chronometer.prototype.startClick = function () {
+        this.currentTime += 1;
+        this.setTime();
+    }, 1000);
+};
 
-// };
+Chronometer.prototype.setMinutes = function () {
+    var minutes = (this.currentTime / 60);
+    return Math.floor(minutes);
+};
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+Chronometer.prototype.setSeconds = function () {
+    var seconds = (this.currentTime % 60);
+    if (this.currentTime === 0){
+        return 0;
+    }
+    return seconds;
+};
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+Chronometer.prototype.twoDigitsNumber = function (value) {
+    if (value < 10){
+        return ("0" + value.toString());
+    }
+    return value.toString();
+};
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
-
-// Chronometer.prototype.setTime = function () {
-
-// };
+Chronometer.prototype.setTime = function () {
+   this.minutes = this.twoDigitsNumber(this.setMinutes());
+   this.seconds = this.twoDigitsNumber(this.setSeconds());
+   console.log(this.seconds);
+};
 
 // Chronometer.prototype.setMilliseconds = function () {
 
 // };
 
-// Chronometer.prototype.stopClick = function () {
-  
-// };
+Chronometer.prototype.stopClick = function () {
+    clearInterval(this.intervalId);
+};
 
-// Chronometer.prototype.resetClick = function () {
-
-// };
+Chronometer.prototype.resetClick = function () {
+    this.currentTime = 0;
+    this.setTime();
+};
 
 // Chronometer.prototype.splitClick = function () {
 
